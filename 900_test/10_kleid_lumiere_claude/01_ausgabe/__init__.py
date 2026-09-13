305844296332221e1c7ee49f829786cf46823913
Wschrenker <jijgemodedesign@gmail.com>
2026-09-13 14:00:27 +0200

    chore: Ordner- und Dateinummerierung neu ordnen

--- 900_test/10_kleid_lumiere_claude/01_ausgabe/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Ausgabe neutraler Geometrie. Kein Konstruktionswissen."""
-
-from .schnittteil import Schnittteil, Linie, Beschriftung  # noqa: F401
-from .dxf import schreibe_dxf, LAYER  # noqa: F401
--- 900_test/10_kleid_lumiere_claude/03_masse/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Maszregister - Koerpermasze aus freigegebenen Quellen."""
-
-from .groessentabelle import (  # noqa: F401
-    DOB_GROESSEN,
-    DOB_TABELLE,
-    Koerpermasse,
-    koerpermasse,
-)
